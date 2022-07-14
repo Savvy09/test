64b94dc61d9c5b26fa1192efbf88bb77b6663227
Savvy09 <108875407+Savvy09@users.noreply.github.com>
2022-07-14 22:08:20 +0530

    Update commands.py

--- test_bot/extensions/commands.py
+++ test_bot/extensions/commands.py
@@ -11,30 +11,23 @@ class Rem(commands.Cog):
     def __init__(self, bot: TestBot) -> None:
         self.bot = bot
         self.background_loop_new.start()
-        
-    @commands.command(name="help")
-    async def help(self, ctx):
-       await ctx.send("🖕")
 
     @commands.command(name="reminder")
     async def reminder(self, ctx: commands.Context) -> Optional[discord.Message]:
-        SavvyName = self.bot.get_user(730271192778539078) or await self.bot.fetch_user(730271192778539078)
         c = await self.bot.db.create(ctx.author.id)
         if c is False:
             await self.bot.db.delete(ctx.author.id)
             embed1 = discord.Embed(
-                title="<a:_:971005195888771092>  Drop Reminder",
+                title="Drop Reminder",
                 description="**Reminder for Drop:** OFF",
                 color=0xED4337,
             )
-            embed1.set_footer(text='Developed by Savvy#4334', icon_url=SavvyName.avatar.url)
             return await ctx.send(embed=embed1)
         embed2 = discord.Embed(
-            title="<a:_:971005195888771092>  Drop Reminder",
-            description="**Reminder for Drop:** ON\n\n**Suggestions:**\n<:_:996732330909650985>  Consider turning this off before doing mass drops.\n<:_:996732330909650985>  Do `sdrop` to start tracking your drops!",
+            title="Drop Reminder",
+            description="**Reminder for Drop:** ON",
             color=0x3AA346,
         )
-        embed2.set_footer(text='Developed by Savvy#4334', icon_url=SavvyName.avatar.url)
         await ctx.send(embed=embed2)
 
     @commands.Cog.listener("on_message")
